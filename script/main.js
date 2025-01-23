031f99c7beb548130c280471062bed837ab7be02
triton99 <tritth@kaist.ac.kr>
2025-01-23 20:04:28 +0900

    commit

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Em có muốn nghe nhạc hem?', // 'Do you want to play music in the background?'
+        title: 'Em có muốn nghe nhạc hem?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
